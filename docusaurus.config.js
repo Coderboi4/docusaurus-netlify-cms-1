e681667f20c69961f25ae244bfa90f8fae550de4
Coderboi4 <103612036+Coderboi4@users.noreply.github.com>
2023-05-30 15:52:56 +1000

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -92,7 +92,7 @@ module.exports = {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://discord.gg/SeVn76zEpp'
+            'https://discord.gg/SeVn76zEpp',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
